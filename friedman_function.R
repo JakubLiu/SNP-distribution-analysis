954d017d4ed40e64b35b152f8a6ace267d9f00be
Jakub Liu (QBA LIU) <113781664+JakubLiu@users.noreply.github.com>
2023-03-25 19:23:28 +0100

    Add files via upload

---
+++ friedman_function.R
@@ -0,0 +1,51 @@
+# if manual_choose = TRUE then you can choose the file from the pop up menu
+# if manual_choose = FLASE then you have to specify the input file path (file_in)
+
+
+
+f_matrix <- function(manual_choose = TRUE, file_in = 'something'){
+  
+  library(PMCMRplus)
+  library(base)
+  
+  if (manual_choose == TRUE) {
+    
+    chosen_file <- file.choose()
+    genes_matrix <- as.matrix(read.table(chosen_file, sep=" "))
+    genes_matrix[,1] = sub(" ", "", genes_matrix[,1])
+    unique_genes = unique(genes_matrix[,3])
+    final_matrix = matrix(-9, nrow = as.numeric(genes_matrix[1,2]), length(unique_genes))
+    
+    for(i in 1:length(unique_genes)){
+      for(j in 1:genes_matrix[1,2]){
+        final_matrix[j,i] = length(which(subset(genes_matrix, genes_matrix[ ,3] == unique_genes[i])[ ,1] == j))}}
+    
+    colnames(final_matrix) = unique_genes
+    final_matrix_transposed = t(final_matrix)
+    results = frdAllPairsConoverTest(y = as.numeric(final_matrix_transposed), groups = rep(c(1:ncol(final_matrix_transposed)), each = nrow(final_matrix_transposed)), blocks = rep(1:nrow(final_matrix_transposed), ncol(final_matrix_transposed)) , p.adjust.method = "bonf")
+    print(basename(chosen_file))
+    results
+  }
+  else {
+    genes_matrix <- as.matrix(read.table(file_in, sep=" "))
+    genes_matrix[,1] = sub(" ", "", genes_matrix[,1])
+    unique_genes = unique(genes_matrix[,3])
+    final_matrix = matrix(-9, nrow = as.numeric(genes_matrix[1,2]), length(unique_genes))
+    
+    for(i in 1:length(unique_genes)){
+      for(j in 1:genes_matrix[1,2]){
+        final_matrix[j,i] = length(which(subset(genes_matrix, genes_matrix[ ,3] == unique_genes[i])[ ,1] == j))}}
+    
+    colnames(final_matrix) = unique_genes
+    final_matrix_transposed = t(final_matrix)
+    results = frdAllPairsConoverTest(y = as.numeric(final_matrix_transposed), groups = rep(c(1:ncol(final_matrix_transposed)), each = nrow(final_matrix_transposed)), blocks = rep(1:nrow(final_matrix_transposed), ncol(final_matrix_transposed)) , p.adjust.method = "bonf")
+    print(basename(file_in))
+    results
+  }
+}
+
+
+
+print(f_matrix(manual_choose = F, file_in = 'C:/Users/Lenovo/Desktop/STUDIA/BIOINFORMATYKA/MASTER/CANONICAL/CHROMOSOME_1/CHR1/EXONS/results_dir/EXONS_canonical_human_chr1_genes_with_4_exons.txt'))
+
+print(f_matrix(manual_choose = TRUE))
---
+++ plot_generator.py
@@ -0,0 +1,40 @@
+# input_file_path   ===> path to the input .txt file (for example Python_input_genes_with_23_exons.txt)
+# output_file_path  ===> file to the output file (a pdf plot)
+# introns_or_exons  ===> 'introns' or 'exons'
+# number  ===> the number of introns or exons (for example 23)
+
+def occurence_counter(input_file_path, output_file_path, introns_or_exons, number):
+    
+    file = open(input_file_path, 'r')
+    data = file.read()
+    data_list = data.split("\n")
+    data_list.pop(-1)
+    file.close()
+    
+    holder_set = set(data_list)
+    uniq_list = list(holder_set)
+    uniq_list_int = []
+
+    for elem in uniq_list:
+        elem2 = int(elem)
+        uniq_list_int.append(elem2)
+
+    sorted_uniq_list = sorted(uniq_list_int)
+    occurence_list = []
+    
+    for i in sorted_uniq_list:
+        element = data_list.count(str(i))
+        occurence_list.append(element)
+        
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    plt.figure(figsize=(10,5))
+    plt.bar(sorted_uniq_list, occurence_list, color = 'blue', width = 0.4)
+    plt.xlabel(introns_or_exons)
+    plt.ylabel('number of SNPs')
+    title = 'genes with' + ' ' + str(number) + ' ' + introns_or_exons
+    plt.title(title)
+    plt.savefig(output_file_path, format="pdf", bbox_inches="tight")
+    plt.show()
+
